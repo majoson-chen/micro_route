8c98ba277dc3804c3a6037121f28f28f7e019330
M-Jay <1376031164@qq.com>
2021-03-13 22:50:08 +0800

    add Licens description

--- micro_route.py
+++ micro_route.py
@@ -1,3 +1,16 @@
+# Copyright 2021 github@Li-Lian1069 m-jay.cn
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# @author m-jay
+# @E-mail m-jay-1376@qq.com
 """
 在MicroPython上实现的类 Flask web服务器,支持单线程,多线程,阻塞和非阻塞.
 
